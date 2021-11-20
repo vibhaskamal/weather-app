484c708c237639e4a835cd1c4a4b1a1ce211f8d2
Vibhas K <vibhaskamalhome@gmail.com>
2021-11-20 21:00:53 +0530

    Created weather and favourites pages

--- src/App.js
+++ src/App.js
@@ -1,93 +1,33 @@
-import React, { useState } from 'react';
-import { api_key } from './data.js';
+import React from 'react';
 import './App.css';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { BrowserRouter, Link } from "react-router-dom";
-import Weather from './components/weather.js';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Weather from './pages/weather.js';
+import Favourites from './pages/favourites';
 
 function App() {
-  let [userInput, setUserInput] = useState('');
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [weatherData, setWeatherData] = useState({});
-  // const [apiError, setApiError] = useState({});
-
-  function handleInputChange(e) {
-    setUserInput(e.target.value);
-  }
-
-  function handleSubmit() {
-    console.log('userInput: ', userInput);
-  }
-
-  function handleClear() {
-    setUserInput('');
-  }
-
-  async function getLocationWeather(city) {
-    try {
-      const result = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=5&appid=${api_key}`);
-
-      if (result.status === 200) {
-        let result_data = await result.json();
-        const lat = result_data[0].lat;
-        const lon = result_data[0].lon;
-        console.log('lat lon: ', result_data[0].lat, result_data[0].lon);
-        getWeatherData(lat, lon);
-        return { success: true, data: await result.json() };
-      }
-
-      return { success: false, error: result.statusText };
-    } catch (ex) {
-      return { success: false, error: ex.message };
-    }
-  }
-
-  async function getWeatherData(lat, lon) {
-    try {
-      const result = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${api_key}`);
-
-      if (result.status === 200) {
-        let result_data = await result.json();
-        console.log('result: ', result_data);
-        return { success: true, data: await result.json() };
-      }
-
-      return { success: false, error: result.statusText };
-    } catch (ex) {
-      return { success: false, error: ex.message };
-    }
-  }
-
   return (
     <BrowserRouter>
       <div className="App">
         <AppBar position="static">
           <Toolbar variant="dense">
             <Typography variant="h6" color="inherit" component="div">
-              <Link  to="/" style={{ color: 'white', 'textDecoration': 'none' }} underline="none">Boolean-it</Link >
+              <Link to="/" style={{ color: 'white', 'textDecoration': 'none' }} underline="none">Weather</Link >
             </Typography>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <Typography variant="h6" color="inherit" component="div">
-              <Link  to="/instructions" style={{ color: 'white', 'textDecoration': 'none' }} underline="none">Instructions</Link >
+              <Link to="/favourites" style={{ color: 'white', 'textDecoration': 'none' }} underline="none">Favourites</Link >
             </Typography>
           </Toolbar>
         </AppBar>
-        {/* Enter name of city:<br />
-        <br />
-        <TextField id="standard-basic" value={userInput} onChange={handleInputChange} label="Standard" variant="standard" />
-        <br /> <br />
-        <Button variant="contained" onClick={handleSubmit}>Submit</Button>
-        &nbsp;&nbsp;&nbsp;
-        <Button variant="contained" onClick={handleClear}>Clear</Button>
-        &nbsp;&nbsp;&nbsp;
-        <Button variant="contained" onClick={() => getLocationWeather(userInput)}>Search</Button>
-        <br /><br /> */}
-        <Weather />
-
+        <Routes>
+          <Route path="/" element={<Weather />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+        {/* <Weather />
+        <Favourites /> */}
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/favourites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Favourites() {
+    return (
+        <div className="App">
+            Favourites
+        </div>
+    );
+}
+
+export default Favourites;
